304148b87264d77599bf394a8f6d1946567d548b
Kaif Ahmad <38567604+kaifahmad@users.noreply.github.com>
2023-06-29 21:26:43 +0530

    File writing completed

--- modules/Data-loader/run.py
+++ modules/Data-loader/run.py
@@ -7,29 +7,36 @@
 import json
 import os
 import requests
-# from datetime import date,time,timedelta,timezone
-from datetime import datetime
+from datetime import datetime,timedelta
+from util import write_to_csv, fetch_data
 
-headers = {'authorization': 'enctoken Lq0JF4X55rHl8mKpcVR1N5lwCwZnmAtoV1/VNJntGyvYFqenLcg74dXTax5zEopb4OfHmaEHiV7SlA80BtZNY0XLEUUUojCvlGIJ4XN8kcfRmPyqtzkZlg=='}
-x = requests.get(
-    # url = 'https://kite.zerodha.com/oms/instruments/historical/256265/15minute?user_id=QV8945&oi=1&from=2022-01-01&to=2022-06-01',
-    url = 'https://kite.zerodha.com/oms/instruments/historical/256265/15minute?user_id=QV8945&oi=1&from=2022-01-01&to=2022-06-01',
-    headers=headers
-    )
-respose = json.loads(x.text)
+
+with open('./config/param_config.json','r') as f:
+  url_config = json.load(f)
+
+i_date = url_config['from_date']
+to_date = url_config['to_date']
+while (datetime.strptime(i_date,'%Y-%m-%d') <= datetime.strptime(to_date,'%Y-%m-%d')):
+   start = i_date
+   end_obj = datetime.strptime(i_date,'%Y-%m-%d') + timedelta(days=60)
+   end = end_obj.strftime('%Y-%m-%d')
+   print(start,end)
+   response_dict = fetch_data(start, end)
+   write_to_csv(candles=response_dict["data"]["candles"]) 
+   i_date = (end_obj + timedelta(days = 1)).strftime('%Y-%m-%d')
+    
+   
 # print(respose["data"]["candles"])
-print (len(respose["data"]["candles"]))
-dt_string = "2023-06-29T15:00:00+0530"
-dt_object1 = datetime.strptime(dt_string, "%Y-%m-%dT%H:%M:%S%z")
+# print (len(respose["data"]["candles"]))
+# dt_string = "2023-06-29T15:00:00+0530"
+# dt_object1 = datetime.strptime(dt_string, "%Y-%m-%dT%H:%M:%S%z")
 # print("dt_object1 =", dt_object1)
 # print("dt_object1 =", dt_object1.year, dt_object1.weekday())
+#    "status": "error",
+# "status": "success",
 
 if not os.path.exists('./output'):
     os.mkdir('./output')
 
-for candle in respose["data"]["candles"]:
-    f = open("./output/nifty.csv", "a")
-    f.write(",".join(str(c) for c in candle))
-    f.write("\n")
-    f.close()
+
         
---
+++ modules/Data-loader/util.py
@@ -0,0 +1,30 @@
+import json
+
+import requests
+
+def fetch_data(start, end)-> dict:
+    """
+    Fetches data from the API
+    """ 
+    BASE_URL = "https://kite.zerodha.com/oms/instruments/historical"
+    with open('./config/param_config.json','r') as f:
+        url_config = json.load(f)
+    headers = {'authorization': url_config["token"]}
+    print(f"{BASE_URL}/{url_config['zerodha_code']}/{url_config['time_frame']}?user_id={url_config['user_id']}&oi=1&from={start}&to={end}")
+    x = requests.get(
+        url = f"{BASE_URL}/{url_config['zerodha_code']}/{url_config['time_frame']}?user_id={url_config['user_id']}&oi=1&from={start}&to={end}",
+        headers=headers
+        )
+    return json.loads(x.text)
+# https://kite.zerodha.com/oms/instruments/historical/256265/15minute?user_id=QV8945&oi=1&from=2023-04-29&to=2023-05-29
+
+
+def write_to_csv(candles:list):
+    """
+    Writes data to the csv file
+    """
+    for candle in candles:
+        f = open("./output/nifty.csv", "a")
+        f.write(",".join(str(c) for c in candle))
+        f.write("\n")
+        f.close()
