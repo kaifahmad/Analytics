a74189bc85f6c0c666d612c269876ae0db8ef7ba
Kaif Ahmad <38567604+kaifahmad@users.noreply.github.com>
2023-06-30 22:55:54 +0530

    Stable commit

--- modules/Data-loader/util.py
+++ modules/Data-loader/util.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import requests
 
@@ -17,11 +18,29 @@ def fetch_data(start, end)-> dict:
     return json.loads(x.text)
 
 
-def write_to_csv(candles:list):
+def write_to_csv(p_candles:list, p_prevClose):
     """
     Writes data to the csv file
     """
-    for candle in candles:
+    i_date = p_candles[0][0].split('T')[0]
+    close = p_candles[0][4]
+    for candle in p_candles:
+        candle.pop()
+
+        dt_object1 = datetime.strptime(candle[0], "%Y-%m-%dT%H:%M:%S%z")
+        if not i_date == dt_object1.date():
+            p_prevClose = close
+            i_date = dt_object1.date()
+
+        candle.append(dt_object1.date())
+        candle.append(dt_object1.weekday())
+        candle.append(f"T{dt_object1.hour}_{dt_object1.minute}")
+        candle.append(f"{round(candle[4] - p_prevClose , 2)}")
+        candle.append(f"{round(((candle[4] - p_prevClose)/p_prevClose)*100 , 2)}")
+        print(candle[0],candle[4],p_prevClose)
+        
+        close = candle[4]
+
         f = open("./output/nifty.csv", "a")
         f.write(",".join(str(c) for c in candle))
         f.write("\n")
